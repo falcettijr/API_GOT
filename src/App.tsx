9a45b7160791595ff985fb844a59846d21d91b9e
Sergio Luiz Falcetti <falcettijr@gmail.com>
2024-05-19 17:35:09 -0300

    criacao da logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
+import { useEffect, useState } from "react";
 import CartaoPersonagem from "./components/CartaoPersonagem";
 import "./index.css";
+import axios from "axios";
 
 function App() {
+  const [characterName, setCharacterName] = useState("");
+  const [characterTitulo, setCharacterTitulo] = useState("");
+  const [characterCultura, setCharacterCultura] = useState("");
+  const [characterGenero, setCharacterGenero] = useState("");
+  const [characterAlianca, setCharacterAlianca] = useState("");
+  const [characterNascimento, setCharacterNascimento] = useState("");
+  const [characterMorte, setCharacterMorte] = useState("");
+  const [valorDigitado, setValorDigitado] = useState(0);
+  const [valorPesquisado, setValorPesquisado] = useState(0);
 
-  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValorDigitado(Number(event.target.value));
+  };
+
+  const handleClick = () => {
+    setValorPesquisado(valorDigitado);
+  }
+  const fetchCharacterName = async (id: number) => {
+    try {
+      const response = await axios.get(
+        `https://anapioficeandfire.com/api/characters/${id}`
+      );
+      setCharacterName(response.data.name);
+      setCharacterTitulo(response.data.titles[0]);
+      setCharacterCultura(response.data.culture);
+      setCharacterGenero(response.data.gender);
+      setCharacterAlianca(response.data.allegiance[0]);
+      setCharacterNascimento(response.data.born);
+      setCharacterMorte(response.data.died);
+    } catch (error) {
+      console.error("Erro ao buscar o nome do personagem:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCharacterName(valorPesquisado);
+  }, [valorPesquisado]);
+
   return (
     <>
       <div className="bg-slate-900 text-neutral-100 flex items-center justify-center gap-4 flex-col">
         <h1 id="title" className="font text-4xl">
-          Game of Thrones API Pesquisa
+          Game of Thrones API - Pesquisa
         </h1>
         <p>Digite um número até 2134:</p>
-        <input
-          id="input"
-          type="number"
-          className="text-slate-900"
-        ></input>
-        <button className="m-4">PESQUISAR!</button>
+        <input id="input" type="number" className="text-slate-900" onChange={handleInputChange}></input>
+        <button className="m-4" onClick={handleClick}>PESQUISAR!</button>
       </div>
       <div className="cartoes-personagem flex gap-4">
-        <CartaoPersonagem />
+        <CartaoPersonagem
+          nome={characterName}
+          cultura={characterCultura}
+          genero={characterGenero}
+          titulo={characterTitulo}
+          alianca={characterAlianca}
+          nascimento={characterNascimento}
+          morte={characterMorte}
+        />
       </div>
     </>
   );
--- src/components/CartaoPersonagem.tsx
+++ src/components/CartaoPersonagem.tsx
@@ -1,23 +1,39 @@
-import nedImage from '../assets/imgs/ned.jpg';
 
-const CartaoPersonagem = () => {
+export type Personagem = {
+  nome: string;
+  titulo: string;
+  cultura: string;
+  genero: string;
+  alianca: string;
+  nascimento: string;
+  morte: string;
+}
+
+const CartaoPersonagem = ({nome, titulo, cultura, genero, alianca, nascimento, morte}: Personagem) => {
+  nome
+  titulo
+  cultura
+  genero
+  alianca
+  nascimento
+  morte
+
   return (
-    <div className="cartao-personagem_container items-center flex border-2 border-solid rounded-lg border-slate-900 p-4 m-2 gap-8 w-screen">
-      <div className="cartao-personagem_imagem"><img src={nedImage}/></div>
-      <div className='cartao-personagem_dados m-2 border-2 border-solid rounded-lg border-slate-900 p-4'>
-        <div className="cartao-personagem_nome_titulo text-lg font-bold">Nome: <div className="cartao-personagem_nome_valor text-sm font-normal"> Ned Stark</div></div>
+    <div className="cartao-personagem_container items-center flex border-2 border-solid rounded-lg border-slate-900 p-8 m-2 gap-8 w-screen justify-center">
+      <div className='cartao-personagem_dados m-2 w-2/5 border-2 border-solid rounded-lg border-slate-900 p-4'>
+        <div className="cartao-personagem_nome_titulo text-lg font-bold">Nome: <div className="cartao-personagem_nome_valor text-sm font-normal">{nome}</div></div>
 
-        <div className="cartao-personagem_titulo_titulo text-lg font-bold">Título: <div className="cartao-personagem_titulo_valor text-sm font-normal">Sor</div></div>
+        <div className="cartao-personagem_titulo_titulo text-lg font-bold">Título: <div className="cartao-personagem_titulo_valor text-sm font-normal">{titulo}</div></div>
 
-        <div className="cartao-personagem_cultura_titulo text-lg font-bold">Cultura: <div className="cartao-personagem_cultura_valor text-sm font-normal">Nortenho</div></div>
+        <div className="cartao-personagem_cultura_titulo text-lg font-bold">Cultura: <div className="cartao-personagem_cultura_valor text-sm font-normal">{cultura ? cultura : 'Não Disponível'}</div></div>
 
-        <div className="cartao-personagem_genero_titulo text-lg font-bold">Gênero: <div className="cartao-personagem_genero_valor text-sm font-normal">Male</div></div>
+        <div className="cartao-personagem_genero_titulo text-lg font-bold">Gênero: <div  className="cartao-personagem_genero_valor text-sm font-normal">{genero === 'Male' && 'Masculino' || genero === 'Female' && 'Feminino' || genero ? genero : 'Não Disponível'}</div></div>
 
-        <div className="cartao-personagem_nascimento_titulo text-lg font-bold">Nascimento: <div className="cartao-personagem_nascimento_valor text-sm font-normal">232 dc</div></div>
+        <div className="cartao-personagem_nascimento_titulo text-lg font-bold">Nascimento: <div className="cartao-personagem_nascimento_valor text-sm font-normal">{nascimento ? nascimento : 'Não Disponível'}</div></div>
 
-        <div className="cartao-personagem_morte_titulo text-lg font-bold">Morte: <div className="cartao-personagem_morte_valor text-sm font-normal">-</div></div>
+        <div className="cartao-personagem_morte_titulo text-lg font-bold">Morte: <div className="cartao-personagem_morte_valor text-sm font-normal">{morte ? morte : 'Vivo'}</div></div>
         
-        <div className="cartao-personagem_alianca_titulo text-lg font-bold">Aliança: <div className="cartao-personagem_alianca_valor text-sm font-normal">Casa Stark</div></div>
+        <div className="cartao-personagem_alianca_titulo text-lg font-bold">Aliança: <div className="cartao-personagem_alianca_valor text-sm font-normal">{alianca}</div></div>
       </div>
     </div>
   )
